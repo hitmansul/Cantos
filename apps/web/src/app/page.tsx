d21b25c03e49f03ff60a5b5228c8bfd0e3a12a95
hitmansul <hitmansul@gmail.com>
2026-07-26 09:12:19 -0300

    feat: integrate command center into home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,13 @@
+import { HomeCommandCenter } from '@/components/HomeCommandCenter';
 import Home from '@/views/Home';
 
 export default function Page() {
-  return <Home />;
+  return (
+    <>
+      <div className="mx-auto w-full max-w-7xl px-4 pt-6">
+        <HomeCommandCenter />
+      </div>
+      <Home />
+    </>
+  );
 }
